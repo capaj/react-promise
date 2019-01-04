d8e1a4bf6421502fd32b376e7b0a2503c9b5f380
Jiri Spac <capajj@gmail.com>
2019-01-04 01:05:41 +0100

    updates deps and adds UMD bundle

--- showcase/webpack.config.js
+++ showcase/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015', 'react']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
